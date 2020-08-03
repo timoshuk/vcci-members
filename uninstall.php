1c79e4fddf76e97944c37fcda9034873f029870f
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-08-03 10:15:06 +0300

    Add database prefix to uninstall.php query

--- uninstall.php
+++ uninstall.php
@@ -18,6 +18,16 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 
 global $wpdb;
 
-$wpdb->query("DELETE FROM wp_posts WHERE post_type = 'vcci_members'");
-$wpdb->query("DELETE FROM wp_postmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)");
-$wpdb->query("DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT id FROM wp_posts)");
+    $root = realpath($_SERVER["DOCUMENT_ROOT"]);
+    require "$root/wp-blog-header.php";
+
+    function get_table_prefix() {
+        global $wpdb;
+        $table_prefix = $wpdb->prefix . "outsider_plugin";
+        return $table_prefix;
+    }
+    $vcci_prefix = get_table_prefix();
+
+$wpdb->query("DELETE FROM '$vcci_prefix . _posts' WHERE post_type = 'vcci_members'");
+$wpdb->query("DELETE FROM '$vcci_prefix . _postmeta' WHERE post_id NOT IN (SELECT id FROM wp_posts)");
+$wpdb->query("DELETE FROM '$vcci_prefix . _term_relationships' WHERE object_id NOT IN (SELECT id FROM wp_posts)");
