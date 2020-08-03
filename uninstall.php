ef7ba79cd2c28e30e24756ec21d4d3cab77438a4
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2020-08-03 10:37:49 +0300

    add ->prefix

--- uninstall.php
+++ uninstall.php
@@ -18,19 +18,10 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 
 global $wpdb;
 
-    $root = realpath($_SERVER["DOCUMENT_ROOT"]);
-    require "$root/wp-blog-header.php";
 
-    function get_table_prefix() {
-        global $wpdb;
-        $table_prefix = $wpdb->prefix . "outsider_plugin";
-        return $table_prefix;
-    }
-    $vcci_prefix = get_table_prefix();
-
-    $vcci_post_table = $vcci_prefix . '_posts';
-    $vcci_postmeta_table = $vcci_prefix . '_postmeta';
-    $vcci_post_term_table = $vcci_prefix . '_term_relationships';
+    $vcci_post_table = $wpdb->prefix . '_posts';
+    $vcci_postmeta_table = $wpdb->prefix . '_postmeta';
+    $vcci_post_term_table = $wpdb->prefix . '_term_relationships';
 
 $wpdb->query("DELETE FROM $vcci_post_table WHERE post_type = 'vcci_members'");
 $wpdb->query("DELETE FROM $vcci_postmeta_table WHERE post_id NOT IN (SELECT id FROM wp_posts)");
